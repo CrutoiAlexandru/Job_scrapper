baea7203b7821a7cf48943acb954482b9ce973a1
Crutoi Alexandru <crutoi2001@gmail.com>
2021-12-09 12:51:43 +0200

    Get the number of jobs on linkedin

--- scrappers/linkedin/linkedin_scrapper.py
+++ scrappers/linkedin/linkedin_scrapper.py
@@ -1,21 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
+import webbrowser
 
-def demo():
-    # get user wanted job
-    user_job = "software developer"#input("job: ")
-    # get user wanted location
-    user_location = "cluj"#input("location: ")
-    
-    # create the item to search for
+def set_search_item(user_job, user_location):
+    # create the global item to search for
+    global search_item
+    # update the item to search for with user provided data
     search_item = f"https://www.linkedin.com/jobs/search?keywords={user_job}&location={user_location}&position=1&pageNum=0"
+
     # make a request to linkedin.com
     req  = requests.get(search_item)
     # make the request a BeautifulSoup element
     soup = BeautifulSoup(req.content, "html.parser")
 
-    # make the request readable
-    soup = soup.prettify()  
+    # print the website were we scrapped the results from
+    print("Website: Linkedin", end = "")
+    # next operation in line is to print the number of jobs on the website
+    number_of_jobs(soup)
 
-    # print the request
-    print(soup)
+def number_of_jobs(soup):
+    # print the number of jobs on the website
+    print("\tNumber of jobs:", soup.find("span", class_="results-context-header__job-count").get_text())
+    print("\n")
+
+def visit():
+    print("Opening: ", search_item)
+    print("\n")
+    # open the website you want to visit in the webbrowser
+    webbrowser.open(search_item)
--- user/input.py
+++ user/input.py
@@ -1,5 +1,27 @@
-import scrappers.linkedin.linkedin_scrapper as scrappers
+import scrappers.linkedin.linkedin_scrapper as linkedin_scrapper
 
 def start():
+    # get user wanted job
+    user_job = "software developer"#input("job: ")
+    # get user wanted location
+    user_location = "cluj"#input("location: ")
+
     # start the demo run on the linkedin scrapper
-    scrappers.demo()
+    linkedin_scrapper.set_search_item(user_job, user_location)
+
+    open_website()
+
+def open_website():
+    # get the website the user wants to visit
+    wanted_website = input("Enter the name of the website you wish to visit(type quit to exit): ")
+    print("\n")
+
+    # check for certain website and open it
+    if wanted_website.lower() == "linkedin": linkedin_scrapper.visit()
+
+    if wanted_website.lower() == "quit": return
+
+    # if the user entered an invalid value check again
+    print("Enter a valid name!")
+    print("\n")
+    open_website()
